02cecb6348434407a5da9d9af5d3a44e5eff666e
JiyeonMoon <jiyeonmoon814@gmail.com>
2020-10-07 22:53:19 +0900

    modified

--- Tutorial/main2.js
+++ Tutorial/main2.js
@@ -15,4 +15,37 @@ btn.addEventListener('click', (e) => {
     console.log(e.target);
     document.querySelector('#my-form')
     .style.background = '#ccc';
+    document.querySelector('body').classList.add('bd-dark');
+    document.querySelector('.items')
+    .lastElementChild.innerHTML='<h1>Hello</h1>'
 });
+
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const eamilInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit',onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+
+    if(nameInput.value===''||eamilInput.value===''){
+        msg.classList.add('error');
+        msg.innerHTML='Please enver all fields';
+
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode
+            (nameInput.value() + ':' + eamilInput.value()));
+
+        userList.appendChild(li);
+
+        //Clear fields
+        nameInput.value='';
+        eamilInput.value='';
+    }
+}
